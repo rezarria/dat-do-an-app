2e2e9c92f390a8a430e3c5fe8e499feafe83c094
rezarria <votanamcb@outlook.com>
2024-06-14 04:38:27 +0700

    chore: Add UploadImage component for image uploading

---
+++ web-quan-ly/components/inputs/UploadImage.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import { LoadingOutlined, PlusCircleOutlined, PlusOutlined } from "@ant-design/icons";
+import { Image, Upload, type UploadProps } from "antd";
+import { useCallback, useState } from "react";
+import { useApi } from "shared";
+
+export type UploadImageProps = {
+	value?: string
+	onChange?: (value: string) => void
+}
+
+export default function UploadImage(props: UploadImageProps) {
+	const { api } = useApi()
+	const [loading, setLoading] = useState(false)
+	const upload = useCallback<NonNullable<UploadProps["customRequest"]>>((options) => {
+		if (api) {
+			let form = new FormData()
+			form.append("image", options.file)
+			api.post<string>("/api/v1/images/form", form, {
+				headers: {
+					"Content-Type": "multipart/form-data"
+				},
+				onDownloadProgress(progressEvent) {
+					options.onProgress?.({ percent: progressEvent.loaded / (progressEvent.total ?? 0) * 100 })
+				},
+			}).then(res => {
+				options.onSuccess?.(res.data)
+			})
+		} else {
+			options.onError?.(new Error("API chưa khởi tạo"))
+		}
+	}, [api])
+
+	const changeHandler = useCallback<NonNullable<UploadProps["onChange"]>>((info) => {
+		switch (info.file.status) {
+			case "done":
+				props.onChange?.(info.file.response)
+				setLoading(false)
+				break
+			case "uploading":
+				setLoading(true)
+				break
+		}
+	}, [props, setLoading])
+
+	return <Upload
+		showUploadList={false} openFileDialogOnClick={props.value == null}
+		listType="picture-card"
+		customRequest={upload} onChange={changeHandler}
+
+	>
+		{
+			props.value ?
+				<div className="w-full h-full overflow-hidden p-[6px]">
+					<Image width={"100%"} height={"100%"} style={{ objectFit: "cover" }} alt="image" wrapperClassName="w-full h-full" src={api?.defaults.baseURL + "/api/v1/images/" + props.value} />
+				</div> :
+				(loading ? <LoadingOutlined /> : <PlusOutlined />)
+		}
+	</Upload>
+}
